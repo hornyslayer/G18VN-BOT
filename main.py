03fc2e711b1193514a8a5375f86b81ba90bd3832
Amrita <hieuduongzx@gmail.com>
2023-04-07 18:31:19 +0700

    123

--- main.py
+++ main.py
@@ -22,32 +22,5 @@ async def on_ready():
 for fn in os.listdir("./cogs"):
     if fn.endswith(".py"):
         bot.load_extension(f"cogs.{fn[:-3]}")
-
-
-@bot.slash_command(name="error-help", description="Cách sửa lỗi cơ bản")
-async def choose_a_error(
-    interaction: nextcord.Interaction,
-    Platform_Name: str = SlashOption(
-        name="platform-name",
-        description="Nền tảng bạn đang chơi!",
-        choices={
-            "Joiplay": "joiplay",
-            "PC": "PC",
-            "Android": "Android"
-        },
-    ),
-
-    Error_Name: str = SlashOption(
-        name="error-name",
-        description="Chọn loại lỗi bạn đang gặp phải!",
-        choices={
-            "RTP VXAce": "Cài đặt RTP VXAce",
-            "Font chữ bị lỗi": "Đổi locale, cài font tương ứng"
-        }
-    )
-
-):
-    await interaction.response.send_message(f"{Error_Name}")
-
-
+        
 bot.run(TOKEN)
